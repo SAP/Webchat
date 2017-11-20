ff30669e9084a0aa76b61c7f2b5d954a4d0a0e4a
jhoudan <jerom.houdan@gmail.com>
2017-11-20 16:30:34 +0100

    refactor(core) improve css

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
+import './style.scss'
+
 const Header = () => (
-  <div>
-    {'Header'}
-    {'X'}
+  <div className='Header'>
+    <img className='Header--logo' src='https://blog.recast.ai/wp-content/uploads/2017/05/recast-ai-logo-small.png' />
+
+    <div className='Header--title'>
+      {'My awesome title'}
+    </div>
+
+    <div className='Header--btn'>
+      {'X'}
+    </div>
   </div>
 )
 
