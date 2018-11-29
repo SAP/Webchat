57104111848690dd1d3bb2e247910df3072f5784
Olivier NGUYEN <olivier.nguyen@recast.ai>
2018-11-29 14:31:40 +0100

    fix(ActionDelay) fix condition isTyping

--- src/components/Live/index.js
+++ src/components/Live/index.js
@@ -92,15 +92,19 @@ class Live extends Component {
     const { showTyping } = this.state
     const lastMessage = messages.slice(-1)[0]
 
-    const sendMessagePromiseCondition = lastMessage && (pathOr(false, ['data', 'hasDelay'], lastMessage)
-      ? pathOr(false, ['data', 'hasNextMessage'], lastMessage)
-      : lastMessage.participant.isBot === false)
+    const sendMessagePromiseCondition =
+      lastMessage &&
+      (pathOr(false, ['data', 'hasDelay'], lastMessage)
+        ? pathOr(false, ['data', 'hasNextMessage'], lastMessage)
+        : lastMessage.participant.isBot === false)
     const pollMessageCondition = lastMessage && pathOr(false, ['attachment', 'delay'], lastMessage)
-    const shouldDisplayTyping =
-      lastMessage && (sendMessagePromiseCondition || pollMessageCondition) &&
+    const shouldDisplayTyping = !!(
+      lastMessage &&
+      (sendMessagePromiseCondition || pollMessageCondition) &&
       !lastMessage.retry &&
       !lastMessage.isSending &&
       showTyping
+    )
 
     return (
       <div
