a6250790cc2cd54e68dd32bac7151648c4abd339
Olivier NGUYEN <olivier.nguyen@recast.ai>
2018-12-19 11:13:36 +0100

    upd(Performance) reduce bundle size + fix input size on IE11

--- src/components/Message/QuickReplies.js
+++ src/components/Message/QuickReplies.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Slider from 'react-slick'
-import _map from 'lodash/map'
-import _sum from 'lodash/sum'
+import sum from 'ramda/es/sum'
+import map from 'ramda/es/map'
+import values from 'ramda/es/values'
 
 import { truncate } from 'helpers'
 
@@ -15,12 +16,14 @@ class QuickReplies extends Component {
     showArrow: true,
   }
 
-  componentDidMount() {
-    const widthQuickReplies = _sum(
-      _map(this.buttons, button => {
-        const dimensions = button.getBoundingClientRect()
-        return dimensions.width
-      }),
+  componentDidMount () {
+    const widthQuickReplies = sum(
+      values(
+        map(button => {
+          const dimensions = button.getBoundingClientRect()
+          return dimensions.width
+        }, this.buttons),
+      ),
     )
 
     if (widthQuickReplies <= 270) {
@@ -28,7 +31,7 @@ class QuickReplies extends Component {
     }
   }
 
-  componentWillReceiveProps(nextProps) {
+  componentWillReceiveProps (nextProps) {
     this.setState({ displayQuickReplies: nextProps.isLastMessage })
   }
 
@@ -39,53 +42,51 @@ class QuickReplies extends Component {
     this.setState({ displayQuickReplies: false })
   }
 
-  render() {
+  render () {
     const { content, style } = this.props
     const { displayQuickReplies, showArrow } = this.state
     const { title, buttons } = content
 
     return (
       <div
-        className="RecastAppQuickReplies"
+        className='RecastAppQuickReplies'
         ref={ref => {
           this.container = ref
         }}
       >
         <Text content={title} style={style} />
 
-        {displayQuickReplies &&
-          buttons &&
-          !!buttons.length && (
-            <Slider
-              prevArrow={<PrevArrow />}
-              nextArrow={<NextArrow />}
-              arrows={showArrow}
-              variableWidth
-              speed={200}
-              infinite={false}
-              draggable={false}
-              prevArrow={<PrevArrow />}
-              nextArrow={<NextArrow />}
-              className="RecastAppSlider RecastAppQuickReplies--slider"
-            >
-              {buttons.map((b, i) => (
-                <div
-                  ref={ref => {
-                    this.buttons[i] = ref
-                  }}
-                  key={i}
-                  className="RecastAppQuickReplies--button"
-                  onClick={() => this.doSendMessage({ type: 'quickReply', content: b })}
-                  style={{
-                    border: `1px solid ${style.accentColor}`,
-                    color: style.accentColor,
-                  }}
-                >
-                  {truncate(b.title, 20)}
-                </div>
-              ))}
-            </Slider>
-          )}
+        {displayQuickReplies
+          && buttons
+          && !!buttons.length && (
+          <Slider
+            arrows={showArrow}
+            variableWidth
+            speed={200}
+            infinite={false}
+            draggable={false}
+            prevArrow={<PrevArrow />}
+            nextArrow={<NextArrow />}
+            className='RecastAppSlider RecastAppQuickReplies--slider'
+          >
+            {buttons.map((b, i) => (
+              <div
+                ref={ref => {
+                  this.buttons[i] = ref
+                }}
+                key={i}
+                className='RecastAppQuickReplies--button'
+                onClick={() => this.doSendMessage({ type: 'quickReply', content: b })}
+                style={{
+                  border: `1px solid ${style.accentColor}`,
+                  color: style.accentColor,
+                }}
+              >
+                {truncate(b.title, 20)}
+              </div>
+            ))}
+          </Slider>
+        )}
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import Webchat from './containers/App'
 
 // https://github.com/babel/babel-loader/issues/401
 if (!global._babelPolyfill) {
-  require('babel-polyfill')
+  require('core-js/modules/es6.promise')
+  require('core-js/modules/es6.object.assign')
 }
 
 export default class RecastWebchat extends Component {
