9074b0cf854ca2696417f8b2b8100888fdbb14de
i321222 <joyce.zhan@sap.com>
2019-06-14 18:24:03 +0800

    support iframe

--- src/components/Message/Text.js
+++ src/components/Message/Text.js
@@ -7,10 +7,21 @@ import { truncate } from 'helpers'
 import './style.scss'
 
 const Text = ({ content, style }) => {
-  let respond
+  let respond, iframeSrc, iframeWidth, iframeHeight, iframeScroll
 
   if (typeof content === 'string') {
     respond = content
+    if(respond.indexOf("iframe")!=-1){
+       // var str = '<iframe src="/xi/ui/offcyclecomp/pages/spotAwardUserIndexMobile.xhtml" width="100%" height="400" scrolling="no"/>';
+        var re ='<iframe src="(.*)" width="(.*)" height="(.*)" scrolling="(.*)"/>';
+        var p = new RegExp(re, "gm");
+        var myArray = respond.match(p);
+        iframeSrc = RegExp.$1;
+        iframeWidth = RegExp.$2;
+        iframeHeight = RegExp.$3;
+        iframeScroll = RegExp.$4;
+        respond = respond.replace(p,'');
+    }
   } else if (typeof content === 'object') {
     respond = JSON.stringify(content)
   } else if (typeof content === 'number') {
@@ -24,6 +35,9 @@ const Text = ({ content, style }) => {
   return (
     <div style={style} className={'RecastAppText CaiAppText'}>
       {respond}
+      {iframeSrc&&
+      (<iframe src={iframeSrc} width={iframeWidth} height={iframeHeight} scrolling={iframeScroll} frameborder="0"/>)
+      }
     </div>
   )
 }
