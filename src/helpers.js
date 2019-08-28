d2cd32f24939f6ce3627587bda0947f7e2be1c68
Wandrocke, John <john.wandrocke@sap.com>
2019-08-28 06:57:28 -0700

    Change method name and refactored code to make cleaner

--- src/helpers.js
+++ src/helpers.js
@@ -7,27 +7,24 @@ export const truncate = (string, length) => {
   return `${string.slice(0, length - 3)}...`
 }
 
-export const storeCredentialsInCookie = (chatId, conversationId, timeToLive) => {
+const conversationKey = 'cai-conversation'
+
+export const storeCredentialsToLocalStorage = (chatId, conversationId, timeToLive) => {
   const payload = { chatId, conversationId }
   const maxAge = 3600 * timeToLive
 
   if (typeof window.localStorage !== 'undefined') {
     // if maxAge is 0 then it never expires.
     // Currently timeToLive is 0.002777777 (~1 sec) if set to never.
-    // Check if never expires or is greater then 60 seconds to save id.
-    if (maxAge === 0 || maxAge >= 60) {
-      const expire = maxAge > 0 ? new Date().getTime() + (maxAge * 1000) : 0
-      const localData = { expire, payload }
-      localStorage.setItem('cai-conversation', JSON.stringify(localData))
-    } else if (localStorage.getItem('cai-conversation')) {
-      localStorage.removeItem('cai-conversation')
-    }
+    const expire = maxAge > 0 ? new Date().getTime() + (maxAge * 1000) : 0
+    const localData = { expire, payload }
+    localStorage.setItem(conversationKey, JSON.stringify(localData))
   }
 }
 
-export const getCredentialsFromCookie = () => {
+export const getCredentialsFromLocalStorage = () => {
   if (typeof window.localStorage !== 'undefined') {
-    const localStorageData = localStorage.getItem('cai-conversation')
+    const localStorageData = localStorage.getItem(conversationKey)
 
     if (localStorageData) {
       try {
@@ -38,7 +35,7 @@ export const getCredentialsFromCookie = () => {
           return localData.payload
         }
         // The data has expired if we got here, so remove it from the storage.
-        localStorage.removeItem('cai-conversation')
+        localStorage.removeItem(conversationKey)
       } catch (err) {} // eslint-disable-line no-empty
     }
   }
