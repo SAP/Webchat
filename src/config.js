9cdccd160d51618d76ed65471e533e0313a6e611
i804074 <john.wandrocke@sap.com>
2020-04-07 11:43:06 -0700

    Fix cody unit test error TypeError: Cannot read property 'getAttribute' of null with the latest WebChat

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = (() => {
   const script = document.currentScript || document.getElementById('cai-webchat')
-  const apiRoot = script.getAttribute('apiRoot') || 'https://api.cai.tools.sap'
+  const apiRoot = (script && script.getAttribute('apiRoot')) || 'https://api.cai.tools.sap'
 
   return {
     apiUrl: `${apiRoot}${apiRoot.slice(-1) === '/' ? '' : '/'}connect/v1`,
