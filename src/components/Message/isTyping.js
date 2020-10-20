57ecc1266ae8515cd1a18d4bd005e31de73635cf
i804074 <john.wandrocke@sap.com>
2020-10-20 13:58:57 -0700

    Use the bot delay plus 2 seconds for busy inicator JIRA: https://sapjira.wdf.sap.corp/browse/SAPMLCONV-13428

--- src/components/Message/isTyping.js
+++ src/components/Message/isTyping.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { sanitizeUrl } from '@braintree/sanitize-url'
+import { propOr } from 'ramda'
 
 import './style.scss'
 
 class IsTyping extends Component {
-  componentDidMount () {
-    const { callAfterTimeout, timeout } = this.props
-    setTimeout(callAfterTimeout, timeout)
+  componentWillUnmount () {
+    if (this._timer) {
+      clearTimeout(this._timer)
+    }
   }
 
   render () {
-    const { image, onImageLoaded } = this.props
-
+    const { image, onImageLoaded, callAfterTimeout } = this.props
+    const timeout = propOr(20000, 'timeout', this.props)
     if (image && sanitizeUrl(image) === 'about:blank') {
       return null
     }
+
+    if (this._timer) {
+      clearTimeout(this._timer)
+      this._timer = null
+    }
+    this._timer = setTimeout(callAfterTimeout, timeout)
 
     return (
       <div className='RecastAppMessage CaiAppMessage bot'>
@@ -26,6 +34,8 @@ class IsTyping extends Component {
   }
 }
 
+// ESLint thinks some PropTypes are not used, but they are retrived in method calls, so disable check
+/* eslint-disable */
 IsTyping.propTypes = {
   image: PropTypes.string,
   callAfterTimeout: PropTypes.func,
