38ea8d526e09352a23522faf3acb470e022ffad3
samcse2007 <51955421+samcse2007@users.noreply.github.com>
2020-12-30 11:52:13 +0530

    Update Picture.js

--- src/components/Message/Picture.js
+++ src/components/Message/Picture.js
@@ -5,10 +5,13 @@ import { sanitizeUrl } from '@braintree/sanitize-url'
 import './style.scss'
 
 const Picture = ({ content, onImageLoaded }) => {
-  if (content && sanitizeUrl(content) === 'about:blank') {
-    return null
-  }
-  return <img onLoad={onImageLoaded} src={content} className={'RecastAppPicture CaiAppPicture'} />
+   if (content && sanitizeUrl(content) === 'about:blank') {
+	return null
+   }
+   if (content.includes('youtube.com/embed')) {
+	return <iframe width='100%' height='auto' src={content} />
+   }
+   return <img onLoad={onImageLoaded} src={content} className={'RecastAppPicture CaiAppPicture'} />
 }
 
 Picture.propTypes = {
